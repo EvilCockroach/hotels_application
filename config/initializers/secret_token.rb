55cbfe78df7692a85412b0f7b163f45d49ac3290
EvilCockroach <oleg-trophymenko@yandex.ua>
2013-07-09 01:44:07 +0300

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotelsApp::Application.config.secret_token = 'ab8afc391b540f85bfef3803c5da2cb7d7e575d3b84157c63a033686a76b16bf2b78f58cb18db2023fb255567c6ca48ff9029ccaaa53c64481495fae9875efb9'
